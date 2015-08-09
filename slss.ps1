d72bd20492e397dafd5b5e6c473287b12e71e8c0
raufguy <raufguy@gmail.com>
2015-08-09 04:05:28 -0400

    slss.ps1
    sls on objs

---
+++ slss.ps1
@@ -0,0 +1,30 @@
+# This function can perform sls / select-string / grep functionality on piped objects which are converted to strings
+
+Function slss($x)
+
+{
+ foreach($a in $input)
+
+  { $a | out-string -stream | select-string $x  }
+
+} #end function slss
+
+<#
+=======
+Example:
+========
+
+PS C:\> get-service | slss ds
+
+Stopped  AppIDSvc           Application Identity                  
+Running  DsmSvc             Device Setup Manager                  
+Stopped  DsRoleSvc          DS Role Server                        
+Stopped  hidserv            Human Interface Device Access         
+Stopped  KdsSvc             Microsoft Key Distribution Service    
+Stopped  lltdsvc            Link-Layer Topology Discovery Mapper  
+Running  LPDSVC             LPD Service                           
+Running  NTDS               Active Directory Domain Services      
+Stopped  SCardSvr           Smart Card                            
+Running  vds                Virtual Disk                          
+Stopped  WDSServer          Windows Deployment Services Server    
+#>
