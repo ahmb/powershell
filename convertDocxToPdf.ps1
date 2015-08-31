88322baabf167ddf8be160691a290524f91c8410
ahmb <raufguy@gmail.com>
2015-08-31 00:18:12 -0400

    Update convertDocxToPdf.ps1

--- convertDocxToPdf.ps1
+++ convertDocxToPdf.ps1
@@ -1,3 +1,5 @@
+#use this to convert single files to pdf
+
 $Word = New-Object -ComObject "Word.Application"
 ($Word.Documents.Open('c:\temp\file.docx')).SaveAs([ref]'c:\temp\file.pdf',[ref]17) 
 $Word.Application.ActiveDocument.Close() 
